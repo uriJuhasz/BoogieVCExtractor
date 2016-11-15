8a6964ebc1c3f3349de35dd14007ac6d86b92062
uri.juhasz@gmail.com <uri.juhasz@gmail.com>
2016-11-15 16:43:31 +0100

    Initial

---
+++ Source/VCExtractor/VCExtractor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Boogie;
+using System;
+
+namespace VCExtractor
+{
+    public class Extractor
+    {
+        public static void extract(Implementation impl)
+        {
+            Console.WriteLine("============Boogie extractor - start");
+            Console.WriteLine("   Implementation: {0}", impl.Name);
+            Console.WriteLine("============Boogie extractor - end");
+        }
+    }
+}
